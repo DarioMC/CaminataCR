133541a75189f3a00d6475fa0232de78d0c13a3d
allexxis <gabr.933@gmail.com>
2017-05-19 16:49:30 -0600

    Cabmios

--- AndroidCaminata/app/src/main/java/com/example/bases/caminatacr/Connection/SpTypes.java
+++
@@ -1,9 +0,0 @@
-package com.example.bases.caminatacr.Connection;
-
-/**
- * Created by Alexis on 5/19/2017.
- */
-
-public enum  SpTypes {
-    LOGIN
-}
--- AndroidCaminata/app/src/main/java/com/example/bases/caminatacr/Data/Hiker.java
+++
@@ -1,9 +0,0 @@
-package com.example.bases.caminatacr.Data;
-
-/**
- * Created by Alexis on 5/19/2017.
- */
-
-public class Hiker {
-    public String alias;
-}
