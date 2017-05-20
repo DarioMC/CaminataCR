9ca09efa88085c594117cf2020183211f9168ba2
Emmanuel Perez Perez <emmanuelperezp@gmail.com>
2017-05-20 04:02:46 -0600

    agregar solucion de la interfaz de windows

---
+++ CaminataCRWindows/AdminPlataforma/InicioSesion.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace AdminPlataforma
+{
+    partial class InicioSesion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonInicioSesion = new System.Windows.Forms.Button();
+            this.textBoxContrasena = new System.Windows.Forms.TextBox();
+            this.linkLabelRegistrarNuevoUsuario = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxUsuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(246)))), ((int)(((byte)(235)))));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.94704F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 54.10592F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.94704F));
+            this.tableLayoutPanel1.Controls.Add(this.buttonInicioSesion, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxContrasena, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.linkLabelRegistrarNuevoUsuario, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxUsuario, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4.165597F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.9724F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.9724F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.9724F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.9724F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.9724F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15.9724F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(285, 288);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // buttonInicioSesion
+            // 
+            this.buttonInicioSesion.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonInicioSesion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.buttonInicioSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonInicioSesion.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonInicioSesion.Location = new System.Drawing.Point(75, 204);
+            this.buttonInicioSesion.Name = "buttonInicioSesion";
+            this.buttonInicioSesion.Size = new System.Drawing.Size(134, 27);
+            this.buttonInicioSesion.TabIndex = 0;
+            this.buttonInicioSesion.Text = "Iniciar Sesion";
+            this.buttonInicioSesion.UseVisualStyleBackColor = false;
+            // 
+            // textBoxContrasena
+            // 
+            this.textBoxContrasena.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBoxContrasena.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(185)))), ((int)(((byte)(181)))));
+            this.textBoxContrasena.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxContrasena.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBoxContrasena.Location = new System.Drawing.Point(68, 162);
+            this.textBoxContrasena.Name = "textBoxContrasena";
+            this.textBoxContrasena.PasswordChar = '*';
+            this.textBoxContrasena.Size = new System.Drawing.Size(148, 20);
+            this.textBoxContrasena.TabIndex = 1;
+            // 
+            // linkLabelRegistrarNuevoUsuario
+            // 
+            this.linkLabelRegistrarNuevoUsuario.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.linkLabelRegistrarNuevoUsuario.AutoSize = true;
+            this.linkLabelRegistrarNuevoUsuario.Font = new System.Drawing.Font("Helvetica LT Std Light", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabelRegistrarNuevoUsuario.LinkColor = System.Drawing.Color.DarkRed;
+            this.linkLabelRegistrarNuevoUsuario.Location = new System.Drawing.Point(94, 249);
+            this.linkLabelRegistrarNuevoUsuario.Name = "linkLabelRegistrarNuevoUsuario";
+            this.linkLabelRegistrarNuevoUsuario.Size = new System.Drawing.Size(95, 30);
+            this.linkLabelRegistrarNuevoUsuario.TabIndex = 3;
+            this.linkLabelRegistrarNuevoUsuario.TabStop = true;
+            this.linkLabelRegistrarNuevoUsuario.Text = "Registrar Nuevo Administrador";
+            this.linkLabelRegistrarNuevoUsuario.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(90, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBoxUsuario
+            // 
+            this.textBoxUsuario.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxUsuario.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(185)))), ((int)(((byte)(181)))));
+            this.textBoxUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxUsuario.Location = new System.Drawing.Point(69, 61);
+            this.textBoxUsuario.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxUsuario.MinimumSize = new System.Drawing.Size(3, 3);
+            this.textBoxUsuario.Name = "textBoxUsuario";
+            this.textBoxUsuario.Size = new System.Drawing.Size(146, 20);
+            this.textBoxUsuario.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(109, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuario";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // InicioSesion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(246)))), ((int)(((byte)(235)))));
+            this.ClientSize = new System.Drawing.Size(309, 312);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "InicioSesion";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonInicioSesion;
+        private System.Windows.Forms.TextBox textBoxUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxContrasena;
+        private System.Windows.Forms.LinkLabel linkLabelRegistrarNuevoUsuario;
+    }
+}
+
