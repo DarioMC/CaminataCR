d711502a028f9d22ed5b68b92f4ad82e7352b0de
Emmanuel Perez Perez <emmanuelperezp@gmail.com>
2017-05-20 18:36:31 -0600

    agregar conexion sql, data accesss layer

---
+++ CaminataCRWindows/ConexionSQL/conexionSQL.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConexionSQL
+{
+
+    public class conexionSQL
+    {
+        private static SqlConnection conexion = new SqlConnection("Server=186.15.160.14,1433; Database=Monchemos; User Id=Emma; password= 0000");
+        private static SqlCommand comandosql;
+        private static SqlDataReader lectorsql;
+
+        public conexionSQL()
+        {
+
+        }
+
+        public void procedimientoEjemplo()
+        {
+            String procedureName = "SPS_SeleccionarAmigos";
+            comandosql = new SqlCommand(procedureName, conexion);
+            comandosql.CommandType = CommandType.StoredProcedure;
+            comandosql.Parameters.Add("@alias", SqlDbType.VarChar).Value = alias;
+            ConectarDb();
+        }
+    }
+}
