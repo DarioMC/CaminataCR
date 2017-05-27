79400c6411cfe1a406f175e6a4d5e231d78051a3
allexxis <gabr.933@gmail.com>
2017-05-27 15:27:08 -0600

    sql queries

--- TableCreationQueries/createAmigo.sql
+++ TableCreationQueries/createAmigo.sql
@@ -4,7 +4,7 @@ CREATE TABLE [Amigo]
 	[IdHiker1] INT  NOT NULL, 
     [IdHiker2] INT NOT NULL, 
 
-	 CONSTRAINT [FK_Hiker_IdPersona1] FOREIGN KEY([IdHiker1]) REFERENCES [dbo].[Hiker] ([IdPersona]),
-	 CONSTRAINT [FK_Hiker_IdPersona2] FOREIGN KEY([IdHiker2]) REFERENCES [dbo].[Hiker] ([IdPersona]),
+	 CONSTRAINT [FK_Hiker_IdPersona1] FOREIGN KEY([IdHiker1]) REFERENCES [dbo].[Hiker] ([IdHiker]),
+	 CONSTRAINT [FK_Hiker_IdPersona2] FOREIGN KEY([IdHiker2]) REFERENCES [dbo].[Hiker] ([IdHiker]),
 	 CONSTRAINT [PK_Amigo] PRIMARY KEY (IdHiker1,IdHiker2)
 )
