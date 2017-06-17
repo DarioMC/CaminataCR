33091656e786d6b297fb5dd8dfdbeaf6633fa2d5
DarioMC <dmcsubotai95@gmail.com>
2017-06-17 06:49:27 -0600

    SPS Agregados2

---
+++ SP/SPS_HabilitarNivelesDificultad.sql
@@ -0,0 +1,13 @@
+﻿
+
+CREATE PROCEDURE dbo.[SPS_HabilitarNivelesDificultad](
+@idDificultad INT
+)
+AS 
+BEGIN
+
+Update NivelDificultad
+SET Habilitado = 1
+WHERE Nivel = @idDificultad
+
+END
---
+++ SP/SPS_HabilitarNivelesPrecio.sql
@@ -0,0 +1,13 @@
+﻿
+
+CREATE PROCEDURE dbo.[SPS_HabilitarNivelesPrecio](
+@idPrecio INT
+)
+AS 
+BEGIN
+
+Update NivelPrecio
+SET Habilitado = 1
+WHERE IdNivelPrecio = @idPrecio
+
+END
